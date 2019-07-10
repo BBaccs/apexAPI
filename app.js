544018733bbea0617477935c18bfa2973ccfcf85
Brian <baccarellab@gmail.com>
2019-07-10 11:45:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ class Apex {
     async getUser() {
         const profileResponse = await fetch(`https://public-api.tracker.gg/v2/apex/standard/search?platform=psn&query=BBaccz&TRN-Api-Key=fb47dc2b-155f-4f8c-b7bf-db83fae4b681`);
         const profile = await profileResponse.json();
+        console.log(profileResponse);
+        console.log(profile)
         return profile;
     }
 }
